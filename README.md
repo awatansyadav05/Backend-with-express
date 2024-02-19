e944e3ace41c7ca3498c66f7e7912b030a3df35f
Awatans Yadav <theavi0509@gmail.com>
2024-02-19 22:40:33 +0530

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-fhdfhgdgh
+In this we will learn about the express with backend with notes.
+
+// REQUEST AND RESPONSE
+req mein saare data hota hai aane waale user ki taraf ka, jaise ki uski location, device info and other things.
+Aur res mein controls hote hai jinke basis pe hum server se response behj paate hain.
+
+//NEXT
+next is just a push so that our request moves to the next thing which should be executed.
+
+//MIDDLEWARE
+middleware ek aisa function hota hai jo route se pahle chalta hain, mtlb saare routes mai koi bhi chale usse phle middleware chalta hain and usmein likha code phle excute hota hain.
+
+//ROUTE PARAMETERS
+--> dynamic routing
+aisa koi bhi route jiska koi hissa baar baara same rhta hai aur kuch chheez uski change hoti rhti hain.. to we will make dynamic route for it.
+== to make any route dynamic you can use : where you want to make dynamic. and to use their value use
+(${`req.params`})
+
+//TEMPLATE ENGINES
---
+++ script.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+app.use(function (req, res, next) {
+//next();
+console.log (req)
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World");const express = require("express");
+  const app = express();
+  
+  app.use(function (req, res, next) {
+    console.log("middleware working");
+  
+    next();
+  });
+  
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
+  app.get("/profile", function (req, res) {
+    res.send("Hello from deadman");
+  });
+  app.get("/profile/:username", function (req, res) {
+    res.send(`hwwlo from ${req.params.username}`);
+  });
+  app.get("/contact", function (req, res) {
+    res.send("location hell ");
+  });
+  
+  app.listen(3000);
+  
+});
+app.get("/profile", function (req, res) {
+  res.send("Hello from deadman");
+});
+app.get("/contact", function (req, res) {
+  res.send("location hell ");
+});
+
+app.listen(3000);
